ab7ad4ce714548e8cafb2c427d658c33f5c5b7a9
diennguyenduy <diennguyenduy1311999@gmail.com>
2020-08-19 15:04:10 +0700

    fix network

--- blockchain/network/scripts/start.sh
+++ blockchain/network/scripts/start.sh
@@ -10,7 +10,8 @@ docker ps
 
 docker exec -it cli ./scripts/channel/createChannel.sh
 
-echo "Joining Bank to channel..."
-docker exec -it cli ./scripts/channel/join-peer.sh peer0 bank BankMSP 7051 1.0
+# Bank was in channel when create channel so don't need to join again
+# echo "Joining Bank to channel..."
+# docker exec -it cli ./scripts/channel/join-peer.sh peer0 bank BankMSP 7051 1.0
 echo "Joining Police to channel..."
 docker exec -it cli ./scripts/channel/join-peer.sh peer0 police PoliceMSP 9051 1.0
--- blockchain/network/scripts/stop.sh
+++ blockchain/network/scripts/stop.sh
@@ -6,5 +6,5 @@ echo "***********************************"
 docker-compose -f ./network/docker-compose-cli.yaml down --volumes --remove-orphans
 rm -r ./network/crypto-config
 rm -r ./network/channel-artifacts
-rm -r ./network/base/docker-compose-base.yaml
-rm -r ./wallet
+# rm -r ./network/base/docker-compose-base.yaml
+# rm -r ./wallet

95b44bc62db06a12d05f700f69e9947149afbac5
TuanQuang <ntquang22298@gmail.com>
2020-08-20 14:09:21 +0700

    fix enroll admin

--- blockchain/src/enrollAdmin.js
+++ blockchain/src/enrollAdmin.js
@@ -2,16 +2,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-'use strict';
+"use strict";
 
-const FabricCAServices = require('fabric-ca-client');
-const { FileSystemWallet, X509WalletMixin } = require('fabric-network');
-const fs = require('fs');
-const path = require('path');
-const User = require('./server/model/User');
-const USER_ROLES = require('./server/configs/constant').USER_ROLES;
+const FabricCAServices = require("fabric-ca-client");
+const { FileSystemWallet, X509WalletMixin } = require("fabric-network");
+const fs = require("fs");
+const path = require("path");
+const User = require("./server/model/User");
+const USER_ROLES = require("./server/configs/constant").USER_ROLES;
 // const mongoose = require('mongoose');
-require('dotenv').config();
+require("dotenv").config();
 
 // Connect database
 // mongoose.connect(
@@ -29,11 +29,11 @@ async function main() {
     const org = args[0];
     const ccpPath = path.resolve(
       __dirname,
-      '..',
-      'connections',
+      "..",
+      "connections",
       `connection-${org}.json`
     );
-    const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
+    const ccpJSON = fs.readFileSync(ccpPath, "utf8");
     const ccp = JSON.parse(ccpJSON);
 
     // Create a new CA client for interacting with the CA.
@@ -51,7 +51,7 @@ async function main() {
     console.log(`Wallet path: ${walletPath}`);
 
     // Check to see if we've already enrolled the admin user.
-    const adminExists = await wallet.exists('admin');
+    const adminExists = await wallet.exists("admin");
     if (adminExists) {
       console.log(
         'An identity for the admin user "admin" already exists in the wallet'
@@ -59,40 +59,40 @@ async function main() {
       return;
     }
 
-    let user;
+    // let user;
 
-    if (org === 'bank') {
-      user = new User({
-        username: process.env.ADMIN_BANK_USERNAME,
-        password: process.env.ADMIN_BANK_PASSWORD,
-        role: USER_ROLES.ADMIN_BANK,
-      });
-    } else if (org === 'police') {
-      user = new User({
-        username: process.env.ADMIN_POLICE_USERNAME,
-        password: process.env.ADMIN_POLICE_PASSWORD,
-        role: USER_ROLES.ADMIN_POLICE,
-      });
-    }
+    // if (org === 'bank') {
+    //   user = new User({
+    //     username: process.env.ADMIN_BANK_USERNAME,
+    //     password: process.env.ADMIN_BANK_PASSWORD,
+    //     role: USER_ROLES.ADMIN_BANK,
+    //   });
+    // } else if (org === 'police') {
+    //   user = new User({
+    //     username: process.env.ADMIN_POLICE_USERNAME,
+    //     password: process.env.ADMIN_POLICE_PASSWORD,
+    //     role: USER_ROLES.ADMIN_POLICE,
+    //   });
+    // }
 
-    let userSaved = await user.save();
-    if (userSaved) {
-      // Enroll the admin user, and import the new identity into the wallet.
-      const upper = org.replace(/^\w/, (c) => c.toUpperCase());
-      const enrollment = await ca.enroll({
-        enrollmentID: 'admin',
-        enrollmentSecret: 'adminpw',
-      });
-      const identity = X509WalletMixin.createIdentity(
-        `${upper}MSP`,
-        enrollment.certificate,
-        enrollment.key.toBytes()
-      );
-      await wallet.import(user.username, identity);
-      console.log(
-        `Successfully enrolled admin user ${user.username} and imported it into the wallet`
-      );
-    }
+    // let userSaved = await user.save();
+    // if (userSaved) {
+    // Enroll the admin user, and import the new identity into the wallet.
+    const upper = org.replace(/^\w/, (c) => c.toUpperCase());
+    const enrollment = await ca.enroll({
+      enrollmentID: "admin",
+      enrollmentSecret: "adminpw",
+    });
+    const identity = X509WalletMixin.createIdentity(
+      `${upper}MSP`,
+      enrollment.certificate,
+      enrollment.key.toBytes()
+    );
+    await wallet.import(org, identity);
+    console.log(
+      `Successfully enrolled admin user and imported it into the wallet`
+    );
+    // }
     process.exit();
   } catch (error) {
     console.error(`Failed to enroll admin user "admin": ${error}`);

fe982385f60da5d1b934b21b7e4eadabdc57e353
dien <diennguyenduy1311999@gmail.com>
2020-09-16 10:10:58 +0700

    fix

--- client/app/src/index.js
+++ client/app/src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-=======
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
 import 'assets/css/material-dashboard-react.css?v=1.8.0';
->>>>>>> 4bfb6650ff82b22e01a4cde7928a271c569bcc5d
 
 ReactDOM.render(
   <Provider store={store}>

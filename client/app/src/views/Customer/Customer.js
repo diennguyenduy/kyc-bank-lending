c702ec44b3849032eab54ef4e59f53dfc20c2819
dien <diennguyenduy1311999@gmail.com>
2020-09-18 11:51:11 +0700

    feat: form and customer pages

---
+++ client/app/src/views/Customer/Customer.js
@@ -0,0 +1,450 @@
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { useSelector, useDispatch } from 'react-redux';
+import * as action from 'actions/producer.actions';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import 'firebase/storage';
+import firebase from 'config/firebase';
+// core components
+
+import Table from 'components/Table/Table.js';
+import Card from 'components/Card/Card.js';
+import CardHeader from 'components/Card/CardHeader.js';
+import CardBody from 'components/Card/CardBody.js';
+import CardIcon from 'components/Card/CardIcon.js';
+import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import PersonIcon from '@material-ui/icons/Person';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import DescriptionIcon from '@material-ui/icons/Description';
+const useStyles = makeStyles((theme) => ({
+  cardCategoryWhite: {
+    '&,& a,& a:hover,& a:focus': {
+      color: 'rgba(255,255,255,.62)',
+      margin: '0',
+      fontSize: '14px',
+      marginTop: '0',
+      marginBottom: '0',
+    },
+    '& a,& a:hover,& a:focus': {
+      color: '#FFFFFF',
+    },
+  },
+  cardTitleWhite: {
+    color: '#FFFFFF',
+    marginTop: '0px',
+    minHeight: 'auto',
+    fontWeight: '300',
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: '3px',
+    textDecoration: 'none',
+    '& small': {
+      color: '#777',
+      fontSize: '65%',
+      fontWeight: '400',
+      lineHeight: '1',
+    },
+  },
+  backdrop: {
+    zIndex: 1,
+    color: 'white',
+  },
+  input: {
+    display: 'none',
+  },
+  appBar: {
+    position: 'relative',
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+  detail: {
+    marginTop: '50px',
+  },
+  center: {
+    textAlign: 'center',
+  },
+  wrap: {
+    wordWrap: 'break-word',
+  },
+}));
+
+export default function Customer() {
+  const storage = firebase.storage();
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const producer = useSelector((state) => state.producer);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const [open, setOpen] = React.useState(false);
+  const [dialog, setDialog] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+  const [deleteAlert, setDeleteAlert] = React.useState(false);
+  const [id, setId] = React.useState('');
+  const [detailDialog, setDetailDialog] = React.useState(false);
+  // const [selectedFile, setSelectedFile] = React.useState(null);
+  const [preview, setPreview] = React.useState(null);
+  const initCustomer = {
+    id: '',
+    username: '',
+    name: '',
+    address: '',
+    description: '',
+    imageUrl: '',
+  };
+  const [customer, setCustomer] = React.useState(initCustomer);
+  useEffect(() => {
+    dispatch(action.getAllCustomer());
+  }, [dispatch]);
+  const columns = [
+    { id: 'name', label: 'Name' },
+    { id: 'address', label: 'Address' },
+  ];
+  // open dialog when user click create button
+  const handleCreateOpen = () => {
+    setDialog('create');
+    setOpen(true);
+  };
+  /*
+    open dialog when user click edit button
+    set data from row table to edit input
+  */
+  const handelEditOpen = (e, row) => {
+    e.stopPropagation();
+    setDialog('edit');
+    setPreview('');
+    console.log(row);
+
+    setCustomer({
+      id: row.id,
+      name: row.name,
+      address: row.address,
+      description: row.description,
+      imageUrl: row.imageUrl,
+      username: row.username,
+    });
+    setOpen(true);
+  };
+  /**
+   * close dialog when user click button close
+   * clear input data
+   **/
+  const handleClose = () => {
+    setPreview('');
+    setCustomer(initCustomer);
+    setOpen(false);
+    setDetailDialog(false);
+  };
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target;
+
+    setCustomer({ ...customer, [name]: value });
+  };
+
+  //create a customer
+  const handleCreateSubmit = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+    customer.imageUrl = preview;
+    setCustomer(customer);
+
+    await dispatch(action.createCustomer(customer));
+    setLoading(false);
+    handleClose();
+  };
+  const handleEditSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    customer.imageUrl = preview !== '' ? preview : customer.imageUrl;
+    setCustomer(customer);
+    await dispatch(action.editCustomer(customer.id, customer));
+    setLoading(false);
+    handleClose();
+  };
+  const handleShowAlert = (e, row) => {
+    e.stopPropagation();
+    setDeleteAlert(true);
+    setId(row.id);
+  };
+
+  const handleDelete = async () => {
+    setLoading(true);
+
+    await dispatch(action.deleteCustomer(id));
+    setLoading(false);
+    setDeleteAlert(false);
+  };
+  const showDetail = (e, row) => {
+    e.stopPropagation();
+    setCustomer({
+      id: row.id,
+      name: row.name,
+      address: row.address,
+      description: row.description,
+      imageUrl: row.imageUrl,
+    });
+    setDetailDialog(true);
+  };
+  const fileChangedHandler = async (e) => {
+    setLoading(true);
+    var file = e.target.files[0];
+    var uploadTask = storage.ref('/customers/' + file.name).put(file);
+    uploadTask.on(
+      'state_changed',
+      (snapShot) => {
+        //takes a snap shot of the process as it is happening
+        console.log(snapShot);
+      },
+      (err) => {
+        //catches the errors
+        console.log(err);
+      },
+      () => {
+        // gets the functions from storage refences the image storage in firebase by the children
+        // gets the download url then sets the image from firebase as the value for the imgUrl key:
+        storage
+          .ref('customers')
+          .child(file.name)
+          .getDownloadURL()
+          .then((fireBaseUrl) => {
+            setPreview(fireBaseUrl);
+          });
+      }
+    );
+
+    setLoading(false);
+  };
+  return (
+    <div>
+      <ToastContainer autoClose={2000} />
+      {/*Customer Table*/}
+      <Card>
+        <CardHeader color='success'>
+          <CardIcon color='info' onClick={() => handleCreateOpen()}>
+            <Icon>add_circle</Icon>
+          </CardIcon>
+          <h4 className={classes.cardTitleWhite}>Customer List</h4>
+        </CardHeader>
+        <CardBody>
+          <Table
+            tableHeaderColor='info'
+            columns={columns}
+            tableData={producer.customerList ? producer.customerList : []}
+            handelEdit={handelEditOpen}
+            handelDelete={handleShowAlert}
+            showDetail={showDetail}
+          />
+        </CardBody>
+      </Card>
+
+      {/*Create and Edit dialog*/}
+      <Dialog
+        fullScreen={fullScreen}
+        open={open}
+        scroll='paper'
+        onClose={handleClose}
+        aria-labelledby='form-dialog-title'
+      >
+        <Backdrop className={classes.backdrop} open={loading}>
+          <CircularProgress color='inherit' />
+        </Backdrop>
+        <DialogTitle id='responsive-dialog-title'>
+          {dialog === 'create' ? 'Create Customer' : 'Edit Customer'}
+        </DialogTitle>
+        <DialogContent>
+          <form>
+            <div style={{ textAlign: 'center' }}>
+              <input
+                accept='image/*'
+                className={classes.input}
+                id='icon-button-file'
+                type='file'
+                onChange={fileChangedHandler}
+              />
+
+              <label htmlFor='icon-button-file'>
+                <IconButton
+                  color='primary'
+                  aria-label='upload picture'
+                  component='span'
+                >
+                  <Avatar
+                    src={preview !== '' ? preview : customer.imageUrl}
+                    style={{
+                      width: '200px',
+                      height: '200px',
+                    }}
+                  />
+                </IconButton>
+              </label>
+            </div>
+            {dialog === 'create' ? (
+              <TextField
+                autoFocus
+                margin='dense'
+                id='username'
+                label='UserName'
+                type='text'
+                fullWidth
+                value={customer.username}
+                onChange={handleChange}
+                name='username'
+              />
+            ) : null}
+            <TextField
+              autoFocus
+              margin='dense'
+              id='name'
+              label='Name'
+              type='text'
+              fullWidth
+              value={customer.name}
+              onChange={handleChange}
+              name='name'
+            />
+            <TextField
+              margin='dense'
+              id='address'
+              label='Address'
+              type='text'
+              fullWidth
+              value={customer.address}
+              onChange={handleChange}
+              name='address'
+            />
+
+            <TextField
+              id='standard-multiline-static'
+              label='Description'
+              multiline
+              rows={12}
+              fullWidth
+              value={customer.description}
+              onChange={handleChange}
+              name='description'
+            />
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color='secondary'>
+            Cancel
+          </Button>
+          <Button
+            onClick={
+              dialog === 'create' ? handleCreateSubmit : handleEditSubmit
+            }
+            color='primary'
+            autoFocus
+          >
+            {dialog === 'create' ? 'Create' : 'Save'}
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/*Detail dialog*/}
+      <Dialog
+        fullWidth
+        fullScreen={fullScreen}
+        open={detailDialog}
+        scroll='paper'
+        onClose={handleClose}
+        aria-labelledby='form-dialog-title'
+      >
+        <Backdrop className={classes.backdrop} open={loading}>
+          <CircularProgress color='inherit' />
+        </Backdrop>
+        <DialogTitle id='responsive-dialog-title'>Customer detail</DialogTitle>
+        <DialogContent>
+          <Avatar
+            src={customer.imageUrl}
+            style={{
+              margin: 'auto',
+              width: '200px',
+              height: '200px',
+            }}
+          />
+          <div className={classes.detail}>
+            <List className={classes.wrap}>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <PersonIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary='Name' secondary={customer.name} />
+              </ListItem>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <LocationOnIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary='Address' secondary={customer.address} />
+              </ListItem>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <DescriptionIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary='Description'
+                  secondary={customer.description}
+                />
+              </ListItem>
+            </List>
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color='primary'>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+      {/**Delete Alert Dialog */}
+      <Dialog
+        open={deleteAlert}
+        onClose={() => setDeleteAlert(false)}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
+      >
+        <Backdrop className={classes.backdrop} open={loading}>
+          <CircularProgress color='inherit' />
+        </Backdrop>
+        <DialogTitle id='alert-dialog-title'>Warning</DialogTitle>
+        <DialogContent>
+          <DialogContentText id='alert-dialog-description'>
+            Are you sure you want to delete this?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setDeleteAlert(false)} color='primary'>
+            Disagree
+          </Button>
+          <Button onClick={handleDelete} color='secondary' autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
---
+++ client/app/src/views/Form/Form.js
@@ -0,0 +1,336 @@
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { useSelector, useDispatch } from 'react-redux';
+import * as action from 'actions/customer.actions';
+import * as producerAction from 'actions/producer.actions';
+import { useHistory } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// core components
+
+import Table from 'components/Table/Table.js';
+import Card from 'components/Card/Card.js';
+import CardHeader from 'components/Card/CardHeader.js';
+import CardBody from 'components/Card/CardBody.js';
+import CardIcon from 'components/Card/CardIcon.js';
+import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import { Avatar } from '@material-ui/core';
+import AppleIcon from '@material-ui/icons/Apple';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles((theme) => ({
+  cardCategoryWhite: {
+    '&,& a,& a:hover,& a:focus': {
+      color: 'rgba(255,255,255,.62)',
+      margin: '0',
+      fontSize: '14px',
+      marginTop: '0',
+      marginBottom: '0',
+    },
+    '& a,& a:hover,& a:focus': {
+      color: '#FFFFFF',
+    },
+  },
+  cardTitleWhite: {
+    color: '#FFFFFF',
+    marginTop: '0px',
+    minHeight: 'auto',
+    fontWeight: '300',
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: '3px',
+    textDecoration: 'none',
+    '& small': {
+      color: '#777',
+      fontSize: '65%',
+      fontWeight: '400',
+      lineHeight: '1',
+    },
+  },
+  backdrop: {
+    zIndex: 1,
+    color: 'white',
+  },
+  input: {
+    display: 'none',
+  },
+  appBar: {
+    position: 'relative',
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+  detail: {
+    marginTop: '50px',
+  },
+  center: {
+    textAlign: 'center',
+  },
+  wrap: {
+    wordWrap: 'break-word',
+  },
+  avatar: {
+    margin: '0 auto 0',
+    overflow: 'hidden',
+    padding: '0',
+    boxShadow:
+      '0 16px 38px -12px rgba(0,0,0,.56), 0 4px 25px 0 rgba(0,0,0,.12), 0 8px 10px -5px rgba(0,0,0,.2)',
+  },
+}));
+
+export default function Form() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const customer = useSelector((state) => state.customer);
+  const producer = useSelector((state) => state.producer);
+  const theme = useTheme();
+  const history = useHistory();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const [open, setOpen] = React.useState(false);
+  const [dialog, setDialog] = React.useState('');
+  const [loading, setLoading] = React.useState(false);
+  const [deleteAlert, setDeleteAlert] = React.useState(false);
+  const [id, setId] = React.useState('');
+  const [product, setProduct] = React.useState('');
+  const initForm = {
+    id: '',
+    name: '',
+    sowingDate: '',
+    harvestDate: '',
+    productId: '',
+  };
+  const [form, setForm] = React.useState(initForm);
+  useEffect(() => {
+    dispatch(action.getAllForm());
+    dispatch(producerAction.getAllProduct());
+  }, [dispatch]);
+  const columns = [
+    { id: 'name', label: 'Name' },
+    { id: 'productId', label: 'Product' },
+  ];
+
+  // open dialog when user click create button
+  const handleCreateOpen = () => {
+    setDialog('create');
+    setOpen(true);
+  };
+  /*
+    open dialog when user click edit button
+    set data from row table to edit input
+  */
+  const handelEditOpen = (e, row) => {
+    e.stopPropagation();
+    setDialog('edit');
+    setProduct(row.productId);
+    setForm(row);
+    setOpen(true);
+  };
+  /**
+   * close dialog when user click button close
+   * clear input data
+   **/
+  const handleClose = () => {
+    setForm(initForm);
+    setOpen(false);
+    setProduct('');
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm({ ...form, [name]: value });
+  };
+
+  //create a form
+  const handleCreateSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    form.productId = product.id;
+    setForm(form);
+    await dispatch(action.createForm(form));
+    setProduct('');
+    setLoading(false);
+    handleClose();
+  };
+  const handleEditSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    form.productId = product.id;
+    setForm(form);
+    await dispatch(action.editForm(form.id, form));
+    setProduct('');
+    setLoading(false);
+    handleClose();
+  };
+  const handleShowAlert = (e, row) => {
+    e.stopPropagation();
+    setDeleteAlert(true);
+    setId(row.id);
+  };
+
+  const handleDelete = async () => {
+    setLoading(true);
+
+    await dispatch(action.deleteForm(id));
+    setLoading(false);
+    setDeleteAlert(false);
+  };
+  const showDetail = async (e, row) => {
+    e.stopPropagation();
+    history.push('/form/' + row.id);
+  };
+  const handleSelect = (event) => {
+    setProduct(event.target.value);
+  };
+
+  return (
+    <div>
+      <ToastContainer autoClose={2000} />
+      {/*Form Table*/}
+      <Card>
+        <CardHeader color='success'>
+          <CardIcon color='info' onClick={() => handleCreateOpen()}>
+            <Icon>add_circle</Icon>
+          </CardIcon>
+          <h4 className={classes.cardTitleWhite}>Form List</h4>
+        </CardHeader>
+        <CardBody>
+          <Table
+            tableHeaderColor='info'
+            columns={columns}
+            tableData={customer.formList ? customer.formList : []}
+            handelEdit={handelEditOpen}
+            handelDelete={handleShowAlert}
+            view={showDetail}
+          />
+        </CardBody>
+      </Card>
+
+      {/*Create and Edit dialog*/}
+      <Dialog
+        fullWidth='true'
+        fullScreen={fullScreen}
+        open={open}
+        scroll='paper'
+        onClose={handleClose}
+        aria-labelledby='form-dialog-title'
+      >
+        <Backdrop className={classes.backdrop} open={loading}>
+          <CircularProgress color='inherit' />
+        </Backdrop>
+        <DialogTitle id='responsive-dialog-title'>
+          {dialog === 'create' ? 'Create Form' : 'Edit Form'}
+        </DialogTitle>
+        <DialogContent>
+          <div style={{ textAlign: 'center' }}>
+            <IconButton
+              color='primary'
+              aria-label='upload picture'
+              component='span'
+            >
+              <Avatar
+                className={classes.avatar}
+                style={{
+                  width: '200px',
+                  height: '200px',
+                  margin: 'auto',
+                }}
+                src={product ? product.imageUrl : ''}
+                alt='Product'
+              >
+                <AppleIcon
+                  style={{
+                    width: '200px',
+                    height: '200px',
+                  }}
+                />
+              </Avatar>
+            </IconButton>
+          </div>
+          <form>
+            <TextField
+              autoFocus
+              margin='dense'
+              id='name'
+              label='Name'
+              type='text'
+              fullWidth
+              value={form.name}
+              onChange={handleChange}
+              name='name'
+            />
+
+            <InputLabel id='demo-simple-select-label'>Product</InputLabel>
+            <Select
+              fullWidth
+              labelId='demo-simple-select-label'
+              id='demo-simple-select'
+              value={product}
+              onChange={handleSelect}
+            >
+              {producer.productList.map((value, index) => (
+                <MenuItem key={index} value={value}>
+                  {value.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color='secondary'>
+            Cancel
+          </Button>
+          <Button
+            onClick={
+              dialog === 'create' ? handleCreateSubmit : handleEditSubmit
+            }
+            color='primary'
+            autoFocus
+          >
+            {dialog === 'create' ? 'Create' : 'Save'}
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      {/**Delete Alert Dialog */}
+      <Dialog
+        open={deleteAlert}
+        onClose={() => setDeleteAlert(false)}
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
+      >
+        <Backdrop className={classes.backdrop} open={loading}>
+          <CircularProgress color='inherit' />
+        </Backdrop>
+        <DialogTitle id='alert-dialog-title'>Warning</DialogTitle>
+        <DialogContent>
+          <DialogContentText id='alert-dialog-description'>
+            Are you sure you want to delete this?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setDeleteAlert(false)} color='primary'>
+            Disagree
+          </Button>
+          <Button onClick={handleDelete} color='secondary' autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}

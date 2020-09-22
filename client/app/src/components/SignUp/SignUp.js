73d855c48f73e55d46c68e32056b356c82d350c7
dien <diennguyenduy1311999@gmail.com>
2020-09-22 16:19:48 +0700

    feat

--- client/app/src/components/SignUp/SignUp.js
+++ client/app/src/components/SignUp/SignUp.js
@@ -17,19 +17,19 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
+    backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3)
+    marginTop: theme.spacing(3),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2)
-  }
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 
 export default function SignUp() {
@@ -111,7 +111,7 @@ export default function SignUp() {
           </Button>
           <Grid container justify='flex-end'>
             <Grid item>
-              <Link href='#' variant='body2'>
+              <Link href='/signIn' variant='body2'>
                 Already have an account? Sign in
               </Link>
             </Grid>
